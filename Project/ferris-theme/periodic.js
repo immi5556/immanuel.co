f92f30cff4ace26b764248d195155e3cf03913b9
Immanuel R <raj@immanuel.co>
2025-04-10 09:31:52 +0530

    more changes.

--- Project/ferris-theme/periodic.js
+++ Project/ferris-theme/periodic.js
@@ -20,7 +20,7 @@ var chemicalElements = [
     { link: 'https://vulgarity-validator.immanuel.co/', symbol: 'ferris-theme/images/vulgr_.png', name: 'Vulgar', group: 12, period: 6 },
     { link: 'https://webrtc-signal-node.immanuel.co/', symbol: 'ferris-theme/images/webrtc_.png', name: 'Webrtc Video Chat', group: 7, period: 6 },
     { link: 'https://aumfish.immanuel.co/', symbol: 'ferris-theme/images/logo_aum1.png', name: 'react js', group: 11, period: 5 },
-    //{ link: 'https://xsfood.org/webrtc/index.html', symbol: 'ferris-theme/images/webrtc_.png', name: 'Screen Share', group: 13, period: 4 },
+    { link: 'https://immanuel.co/debs_game.html', symbol: 'data/ark/ARK_music.png', name: "Deb's Game", group: 13, period: 4 },
     //{ link: 'https://aumfish.immanuel.co/', symbol: 'ferris-theme/images/logo_aum1.png', name: 'react js', group: 14, period: 4 },
     //{ link: 'https://feedback.immanuel.co/', symbol: 'ferris-theme/images/yt_feed.png', name: 'Feedback', group: 15, period: 4 },
     //{ link: 'https://photos.immanuel.co/', symbol: 'ferris-theme/images/a7_.png', name: 'A7 Gallery', group: 16, period: 4 },
